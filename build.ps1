9a64446abd971a27584801ac42ad902c0aff5148
Adam Bertram <adbertram@gmail.com>
2017-03-22 15:03:56 -0500

    Added logging in build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+Write-Host 'Authenticating to Azure...'
 Add-AzureRmAccount
 
 $sharedParams = @{
@@ -6,10 +7,12 @@ $sharedParams = @{
 }
 
 ## Send the changed DSC configuration to Azure
+Write-Host 'Sending DSC configuration to Azure Automation...'
 #Import-AzureRmAutomationDscConfiguration @sharedParams -SourcePath .\NewTestEnvironment.ps1 -Published -Force
 Import-AzureRmAutomationDscConfiguration @sharedParams -SourcePath C:\Dropbox\GitRepos\TestDomainCreator\NewTestEnvironment.ps1 -Published -Force
 
 ## Grab config data from source
+Write-Host 'Getting ConfigData from source...'
 $configDataFilePath = "$env:TEMP\ConfigData.psd1"
 $iwrParams = @{
 	Uri = 'https://raw.githubusercontent.com/adbertram/TestDomainCreator/master/ConfigurationData.psd1'
@@ -20,6 +23,7 @@ Invoke-WebRequest @iwrParams
 $configData = Invoke-Expression (Get-Content -Path $configDataFilePath -Raw)
 
 ## Start the DSC compile in Azure
+Write-Host 'Begin Azure Automation DSC compile...'
 $compParams = $sharedParams + @{
 	ConfigurationName = 'NewTestEnvironment'
 	ConfigurationData = $configData
@@ -27,18 +31,30 @@ $compParams = $sharedParams + @{
 $CompilationJob = Start-AzureRmAutomationDscCompilationJob @compParams
 
 ## Wait for the DSC compile
+Write-Host 'Waiting for Azure Automation DSC compile...'
 while($CompilationJob.EndTime -eq $null -and $CompilationJob.Exception -eq $null)
 {
     $CompilationJob = $CompilationJob | Get-AzureRmAutomationDscCompilationJob
     Start-Sleep -Seconds 3
 }
 
+## Ensure the compile was good
 $CompilationJob | Get-AzureRmAutomationDscCompilationJobOutput -Stream Any
 
-## Apply the DSC configuration to our test VM
+## Assign the configuration to the node?? This might not be necessary
+Write-Host 'Assigning DSC configuraiton to node...'
+$nodeId = (Get-AzureRmAutomationDscNode @sharedParams -Name LABDC).Id
+$node = Set-AzureRmAutomationDscNode -NodeConfigurationName 'NewTestEnvironment.LABDC' -ResourceGroupName 'Group' -Id $nodeId
 
+## Wait for the assignment (running???) to complete
+while($node.Status -ne 'Done???')
+{
+    $node = $node | Get-AzureRmAutomationDscNode
+    Start-Sleep -Seconds 3
+}
+
+## Apply the DSC configuration to our test VM. This might not be necessary if the assign part does it.
 
 
 ## Ensure the DSC configuration was good
-$Node  = Get-AzureAutomationDscNode -Name WEB02
-Get-AzureAutomationDSCNodeReport  -NodeId $Node.ID | Sort EndTime | Select-Object -last 1
+Get-AzureRmAutomationDscNodeReport -NodeId $nodeId @sharedParams | sort endtime | select -last 1
