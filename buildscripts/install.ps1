e3d6fc5d7877cbbae1d84f07fc3921bcdf116235
Adam Bertram <adbertram@gmail.com>
2017-03-22 17:13:07 -0500

    appveyor tweaks

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -1,5 +1,5 @@
 Write-Host 'Installing necessary PowerShell modules...'
-Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
-Import-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
-Install-Module AzureRM -verbose -Force -Confirm:$false
+$null = Install-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+$null = Import-PackageProvider NuGet -MinimumVersion '2.8.5.201' -Force
+Install-Module AzureRM -verbose -Force -Confirm:$false -AllowClobber
 Install-Module Pester -verbose -Force -Confirm:$false
