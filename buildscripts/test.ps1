20b84501347dccbcf478e7de350bd1ba9fd0be63
Adam Bertram <adbertram@gmail.com>
2017-03-31 17:01:08 -0500

    fix for test script

--- buildscripts/test.ps1
+++ buildscripts/test.ps1
@@ -5,4 +5,4 @@ $testResultsFilePath = "$ProjectRoot\TestResults.xml"
 Invoke-Pester -Path "$ProjectRoot\New-TestEnvironment.Tests.ps1" -OutputFormat NUnitXml -OutputFile $testResultsFilePath -EnableExit
 
 $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
-(New-Object 'System.Net.WebClient').UploadFile( $Address, $Source )
+(New-Object 'System.Net.WebClient').UploadFile( $Address, $testResultsFilePath )
